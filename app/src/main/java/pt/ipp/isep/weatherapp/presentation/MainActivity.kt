98a73f396966372bcabc8b917672517a9b0a20c8
Rafael Ribeiro <57731361+rafael-ribeiro1@users.noreply.github.com>
2022-03-05 04:15:27 +0000

    MainActivity design and ViewBinding

--- app/src/main/java/pt/ipp/isep/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/pt/ipp/isep/weatherapp/presentation/MainActivity.kt
@@ -3,10 +3,16 @@ package pt.ipp.isep.weatherapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import pt.ipp.isep.weatherapp.R
+import pt.ipp.isep.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
