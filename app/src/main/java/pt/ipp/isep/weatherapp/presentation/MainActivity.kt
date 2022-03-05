c75d4715d5db2ac2542f7457a273c3384af2aff5
Rafael Ribeiro <57731361+rafael-ribeiro1@users.noreply.github.com>
2022-03-05 01:14:19 +0000

    Changing icon and colors/themes

--- app/src/main/java/pt/ipp/isep/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/pt/ipp/isep/weatherapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package pt.ipp.isep.weatherapp
+package pt.ipp.isep.weatherapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pt.ipp.isep.weatherapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
